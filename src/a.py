a3dfd107a821c146d071147e0ade82e4117f5a0f
Michael Pradel <michael@binaervarianz.de>
2023-08-24 09:24:01 +0200

    Adding file with a warning and a suppression for it

---
+++ src/a.py
@@ -0,0 +1,9 @@
+"""Module docstring"""
+
+def toy():
+    """toy example"""
+    # pylint: disable=undefined-variable
+    var_a = 23
+    var_b = 42
+    var_c = var_a + var_b + var_x
+    return var_c
